bee854898ef47aae607ab84cde189f425ccfb493
woodcrust <roboucrop@gmail.com>
2016-06-09 19:19:01 +0300

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,16 @@
-ShoppingCart::Engine.routes.draw do
+ShoppingCart::Engine.routes.append do
+  resource  :cart, controller: 'cart' do
+    collection do
+      post 'add'
+    end
+  end
+
+  resources :orders,   controller: 'order'
+  resources :checkout, controller: 'checkout'
+
+  resource :settings do
+    collection do
+      patch 'update_password'
+    end
+  end
 end
