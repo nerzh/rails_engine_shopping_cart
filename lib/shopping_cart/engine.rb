1154d1ca57a0f6957f903797d8a54269f40bc5fe
woodcrust <roboucrop@gmail.com>
2016-06-09 19:20:42 +0300

    update engine

--- lib/shopping_cart/engine.rb
+++ lib/shopping_cart/engine.rb
@@ -1,5 +1,22 @@
+require 'aasm'
+require 'wicked'
+require 'slim_form_object'
+require 'cancancan'
+
 module ShoppingCart
   class Engine < ::Rails::Engine
     isolate_namespace ShoppingCart
+
+    initializer 'shopping_cart' do
+      ActiveSupport.on_load(:active_record) do
+        ActiveRecord::Base.extend(Settings::Product)
+        ActiveRecord::Base.extend(Settings::User)
+      end
+
+      ActiveSupport.on_load(:action_controller) do
+        include ShoppingCart::AppCtrlConcern
+        helper ShoppingCart::Engine.helpers
+      end
+    end
   end
 end
