188572e0a2f36115e29e878fd3a7a8ff3b9d8342
woodcrust <roboucrop@gmail.com>
2016-06-12 09:45:56 +0300

    clear routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 ShoppingCart::Engine.routes.append do
-  resource  :cart, controller: 'cart' do
+
+  resource  :cart, controller: 'cart', only: [:show, :update, :destroy] do
     collection do
       post 'add'
     end
   end
 
-  resources :orders,   controller: 'order'
-  resources :checkout, controller: 'checkout'
+  resources :orders,   controller: 'order',    only: [:index, :new]
+  resources :checkout, controller: 'checkout', only: [:show, :update, :destroy]
 
-  resource :settings do
+  resource :settings, only: [:show, :update] do
     collection do
       patch 'update_password'
     end
   end
+
 end
