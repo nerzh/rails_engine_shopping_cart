d2257ac404dbee8488590bc949543b591c5f50ad
OleG <emptystamp@gmail.com>
2017-06-18 13:05:29 +0300

    revert fix

--- lib/shopping_cart/settings.rb
+++ lib/shopping_cart/settings.rb
@@ -5,6 +5,7 @@ module ShoppingCart
   module Settings
     module Product
       def this_is_product
+        ShoppingCart.product_model = self
         has_many :orders,     through: :order_items, class_name: ShoppingCart::OrderItem, foreign_key: "product_id"
         has_many :order_items,                       class_name: ShoppingCart::OrderItem, foreign_key: "product_id"
       end
@@ -12,6 +13,7 @@ module ShoppingCart
 
     module User
       def this_is_user
+        ShoppingCart.user_model = self
         has_one  :shipping_address, dependent: :delete,     class_name: ShoppingCart::ShippingAddress, foreign_key: "user_id"
         has_one  :billing_address,  dependent: :delete,     class_name: ShoppingCart::BillingAddress,  foreign_key: "user_id"
         has_one  :credit_card,      dependent: :delete,     class_name: ShoppingCart::CreditCard,      foreign_key: "user_id"
